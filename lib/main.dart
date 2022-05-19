7d99ec58ad456a3e19de6f03b278fa719910acd5
sproute <prestiz4321@gmail.com>
2022-05-19 19:08:20 +0300

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,13 @@
-// import 'package:flutter/material.dart';
-
-// import 'src/common/my_app.dart';
-// import 'src/data/local_datasource/local_datasource.dart';
-
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   final Database database = Database();
-//   final TodosDao todosDao = TodosDao(database);
-//   runApp(MyApp(database: database, dao: todosDao));
-// }
-
 import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
 
 import 'bootstrap.dart';
-// import 'src/data/local/plugins/shared_preferences_impl.dart';
-// import 'src/data/local/storage_todos_api.dart';
+
 import 'src/data/local_datasource/local_datasource.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final Database database = Database();
   final TodosDao todosDao = TodosDao(database);
-
-  // final todosApi = StorageTodosApi(
-  //   plugin: SharedPreferencesApi(sharedPreferences: pref),
-  // );
 
   bootstrap(todosApi: todosDao);
 }
